e3049907461b200f0d0e02a8d41678dc63da0d13
Yana Krukovska <jana.krua@gmail.com>
2019-02-10 19:37:15 +0200

    Caesar cipher. Initial impl

---
+++ src/ua/edu/ukma/ykrukovska/unit4/practice/CaesarCipher.java
@@ -0,0 +1,36 @@
+package ua.edu.ukma.ykrukovska.unit4.practice;
+
+public class CaesarCipher {
+
+    private String line;
+
+    public void saveString(String line){
+        this.line = line;
+    }
+
+
+
+    public String encode(int shift) {
+
+        if (line == null || line.length() == 0){
+            throw new IllegalStateException("Line to encode is not specified");
+        }
+
+        String result = "";
+        for (int pos = 0; pos < line.length(); pos++) {
+            int c = line.charAt(pos) + shift;
+
+            if (c > 126) {
+                c -= 95;
+            } else if (c < 32) {
+                c += 95;
+            }
+            result += (char) c;
+
+        }
+
+        return result;
+
+
+    }
+}
---
+++ src/ua/edu/ukma/ykrukovska/unit4/practice/CaesarCipherTester.java
@@ -0,0 +1,26 @@
+package ua.edu.ukma.ykrukovska.unit4.practice;
+
+import java.io.IOException;
+
+import static ua.edu.ukma.ykrukovska.unit4.practice.DataInput.getInt;
+import static ua.edu.ukma.ykrukovska.unit4.practice.DataInput.getString;
+
+public class CaesarCipherTester {
+
+    public static void main(String[] args) throws IOException {
+
+        CaesarCipher encoder = new CaesarCipher();
+
+        System.out.println(("Line to encode: "));
+        String lineToEncode = getString();
+        encoder.saveString(lineToEncode);
+        System.out.println("Insert shift: ");
+        int shift = getInt();
+
+
+        System.out.println("Encoded line: " + encoder.encode(shift));
+
+
+    }
+
+}
