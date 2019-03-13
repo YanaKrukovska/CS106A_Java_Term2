eacd50136a7346c368c86764dd709e9fcc9d5e97
Yana Krukovska <jana.krua@gmail.com>
2019-03-13 22:41:31 +0200

    Added scanner to method that gets user's String.

--- src/ua/edu/ukma/ykrukovska/unit9/practice/InputUtils.java
+++ src/ua/edu/ukma/ykrukovska/unit9/practice/InputUtils.java
@@ -1,6 +1,5 @@
 package ua.edu.ukma.ykrukovska.unit9.practice;
 
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,11 +7,11 @@ import java.util.Scanner;
 
 public class InputUtils {
 
-    public static Scanner scanner = new Scanner(System.in);
 
     public static double getUserInput(double min, double max, String prompt) {
+     Scanner scanner = new Scanner(System.in);
         System.out.println("Insert " + prompt);
-        double userInput =  scanner.nextDouble();
+        double userInput = scanner.nextDouble();
 
         if (userInput < min || userInput > max) {
             System.out.println("Wrong value of " + prompt);
@@ -27,14 +26,14 @@ public class InputUtils {
 
 
     public static String getCorrectUserString(String prompt) {
+        Scanner scanner = new Scanner(System.in);
         String userInput = "";
         boolean exitFlag = false;
         do {
             System.out.println("Insert: " + prompt);
-            try {
-                userInput = getString();
-            } catch (IOException e) {
-                e.printStackTrace();
+
+            if (scanner.hasNextLine()) {
+                userInput = scanner.nextLine();
             }
             if (userInput == null || userInput.length() == 0 || userInput.trim().length() == 0) {
                 System.out.println("Wrong value of: " + prompt);
@@ -46,7 +45,7 @@ public class InputUtils {
     }
 
 
-    public static String getString() throws IOException{
+    public static String getString() throws IOException {
         InputStreamReader isr = new InputStreamReader(System.in);
         BufferedReader br = new BufferedReader(isr);
         String s = br.readLine();
