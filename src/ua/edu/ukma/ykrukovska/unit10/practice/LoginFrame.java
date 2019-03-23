a57021e5aad8686265b642f12ee16adf82e1ff3c
Yana Krukovska <jana.krua@gmail.com>
2019-03-23 18:12:32 +0200

    Swing practice. Task:  log in by inserting username and password and get error or special message. Initial impl

---
+++ src/ua/edu/ukma/ykrukovska/unit10/practice/LoginFrame.java
@@ -0,0 +1,69 @@
+package ua.edu.ukma.ykrukovska.unit10.practice;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class LoginFrame extends JFrame {
+
+
+    private JLabel userLabel = new JLabel("Username");
+    private JLabel passwordLabel = new JLabel("Password");
+
+    private JTextField usernameField = new JTextField();
+    private JPasswordField passwordField = new JPasswordField();
+
+    private JButton loginButton = new JButton("Login");
+
+
+    public LoginFrame() {
+
+        setTitle("Log In");
+        setBounds(100, 100, 500, 300);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(null);
+
+
+        userLabel.setBounds(50, 65, 100, 50);
+        usernameField.setBounds(150, 65, 250, 40);
+        passwordLabel.setBounds(50, 115, 100, 50);
+        passwordField.setBounds(150, 115, 250, 40);
+        loginButton.setBounds(200, 200, 100, 30);
+
+
+
+        panel.add(userLabel);
+        panel.add(usernameField);
+        panel.add(passwordLabel);
+        panel.add(passwordField);
+        panel.add(loginButton);
+
+        add(panel, BorderLayout.CENTER);
+
+
+        loginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (e.getSource() == loginButton) {
+                    String username = usernameField.getText();
+                    String password = new String (passwordField.getPassword());
+
+
+                    if (username.equals("Justin") && password.equals("rita")) {
+                        JOptionPane.showMessageDialog(getParent(), "Login Successful. Oleg Vynnyk is better than Justin Bieber");
+                    } else {
+                        JOptionPane.showMessageDialog(getParent(), "Error");
+                    }
+
+                }
+            }
+        });
+
+    }
+
+
+}
+
+
---
+++ src/ua/edu/ukma/ykrukovska/unit10/practice/LoginTest.java
@@ -0,0 +1,19 @@
+package ua.edu.ukma.ykrukovska.unit10.practice;
+
+import javax.swing.*;
+
+
+public class LoginTest {
+
+    public static void main(String[] args) {
+        LoginFrame frame = new LoginFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+
+
+
+
+
+}
