c989079eb3e9d487c18067fa4619044788670fc4
Yana Krukovska <jana.krua@gmail.com>
2019-05-17 11:13:58 +0300

    Deleted unnecessary things

--- src/ua/edu/ukma/ykrukovska/graphApp/GraphAppDemo.java
+++ src/ua/edu/ukma/ykrukovska/graphApp/GraphAppDemo.java
@@ -3,6 +3,6 @@ package ua.edu.ukma.ykrukovska.graphApp;
 public class GraphAppDemo {
 
     public static void main(String[] args) {
-        GraphApp graphApp = new GraphApp();
+        new GraphApp();
     }
 }
