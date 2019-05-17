b3d0706f56553fbc18630af446fcc8248ccc51f6
Yana Krukovska <jana.krua@gmail.com>
2019-05-17 11:13:03 +0300

    Put some calculations in separate methods. Renamed variables to make it more clear

--- src/ua/edu/ukma/ykrukovska/graphApp/GraphCanvas.java
+++ src/ua/edu/ukma/ykrukovska/graphApp/GraphCanvas.java
@@ -6,19 +6,17 @@ import java.awt.geom.Line2D;
 
 public class GraphCanvas extends JComponent {
 
-    private double krok;
-    private double y;
-    private double x;
+    private double step;
     private double xMin;
     private double xMax;
     private double a;
     private static final double SCALE = 50;
 
-    public GraphCanvas(double krokValue, double xMin, double xMax, double a) {
-        this.krok = 0.01;
-        this.xMax = 10;
-        this.xMin = -10;
-        this.a = 1;
+    public GraphCanvas(double step, double xMin, double xMax, double a) {
+        this.step = step;
+        this.xMin = xMin;
+        this.xMax = xMax;
+        this.a = a;
     }
 
     @Override
@@ -28,41 +26,11 @@ public class GraphCanvas extends JComponent {
         Graphics2D gr = (Graphics2D) g;
         gr.setColor(Color.BLACK);
 
-        //Draw X and Y lines
-        gr.drawLine(this.getWidth() / 2, 0, this.getWidth() / 2, getWidth());
-        gr.drawLine(0, this.getHeight() / 2, getWidth(), this.getHeight() / 2);
-
-
-        //X points
-        double kx = 0;
-        for (int c = 0; c < getWidth(); c += 50) {
-            gr.drawString(Double.toString((kx)), ((getWidth() / 2) + c), getHeight() / 2 + 10);
-            kx++;
-        }
-
-        double kxm = 0;
-        for (int c = 50; c < getWidth(); c += 50) {
-            kxm--;
-            gr.drawString(Double.toString((kxm)), ((getWidth() / 2) - c), getHeight() / 2 + 10);
-        }
-
-        //Y points
-        double ky = 0;
-        for (int c = 50; c < getHeight(); c += 50) {
-            ky++;
-            gr.drawString(Double.toString((ky)), (getWidth() / 2), getHeight() / 2 + c);
-
-        }
-
-        double kym = 0;
-        for (int c = 50; c < getHeight(); c += 50) {
-            kym--;
-            gr.drawString(Double.toString((kym)), (getWidth() / 2), getHeight() / 2 - c);
-        }
+        drawLines(gr);
+        drawXPoints(gr);
+        drawYPoints(gr);
 
         gr.setColor(Color.RED);
-
-
 
         double xShift = getWidth() / 2.;
         double yShift = getHeight() / 2.;
@@ -73,7 +41,7 @@ public class GraphCanvas extends JComponent {
         double y2;
 
         do {
-            x2 = x1 + krok;
+            x2 = x1 + step;
             y2 =  (FunctionCalculator.calculate(x2, a)) ;
             gr.draw(new Line2D.Double(xShift + x1*SCALE, yShift - y1*SCALE, xShift+ x2*SCALE, yShift - y2*SCALE));
             System.out.println("x1 = " + x1 + " " + y1);
@@ -83,6 +51,39 @@ public class GraphCanvas extends JComponent {
         } while (x2 <= xMax);
 
 
+    }
+
+    private void drawYPoints(Graphics2D gr) {
+        double yNegative = 0;
+        for (int c = 50; c < getHeight(); c += 50) {
+            yNegative--;
+            gr.drawString(Double.toString((yNegative)), (getWidth() / 2), getHeight() / 2 + c);
+
+        }
+        double yPositive = 0;
+        for (int c = 50; c < getHeight(); c += 50) {
+            yPositive++;
+            gr.drawString(Double.toString((yPositive)), (getWidth() / 2), getHeight() / 2 - c);
+        }
+    }
+
+    private void drawXPoints(Graphics2D gr) {
+        double xPositive = 0;
+        for (int c = 0; c < getWidth(); c += 50) {
+            gr.drawString(Double.toString((xPositive)), ((getWidth() / 2) + c), getHeight() / 2 + 10);
+            xPositive++;
+        }
+
+        double xNegative = 0;
+        for (int c = 50; c < getWidth(); c += 50) {
+            xNegative--;
+            gr.drawString(Double.toString((xNegative)), ((getWidth() / 2) - c), getHeight() / 2 + 10);
+        }
+    }
+
+    private void drawLines(Graphics2D gr) {
+        gr.drawLine(this.getWidth() / 2, 0, this.getWidth() / 2, getWidth());
+        gr.drawLine(0, this.getHeight() / 2, getWidth(), this.getHeight() / 2);
     }
 
 }
